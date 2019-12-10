edfbe501603bcd259dddf52376bf69ced1c6e2bc
Julie Nguyen <julie-nguyen@live.com>
2019-12-10 02:33:35 +0000

    Added in redirects

--- express_server.js
+++ express_server.js
@@ -38,20 +38,25 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars)
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render('urls_show', templateVars);
 });
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});  
 
 
 app.listen(PORT, () => {
